f21a879525cffcc475f83efeab9360285d28bc28
Roman Serdiuk <roman.srdk21@gmail.com>
2025-07-19 14:59:03 -0400

    Pagination

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,5 +1,11 @@
-function LoadMoreBtn() {
-  return <button>Load more</button>;
+import css from "./LoadMoreBtn.module.css";
+
+function LoadMoreBtn({ onClick }) {
+  return (
+    <button onClick={onClick} className={css.loadMoreBtn}>
+      Load more
+    </button>
+  );
 }
 
 export default LoadMoreBtn;
--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectItems } from "../redux/campers/selectors";
 import { getAllCampers } from "../redux/campers/operations";
@@ -12,10 +12,17 @@ import LoadMoreBtn from "../components/LoadMoreBtn/LoadMoreBtn";
 function CatalogPage() {
   const dispatch = useDispatch();
   const campers = useSelector(selectItems);
+  const [campersPerPage, setCampersPerPage] = useState(4);
 
   useEffect(() => {
     dispatch(getAllCampers());
   }, [dispatch]);
+
+  const handleLoadMore = () => {
+    setCampersPerPage((prev) => prev + 4);
+  };
+
+  const visibleCampers = campers.slice(0, campersPerPage);
 
   return (
     <>
@@ -36,8 +43,10 @@ function CatalogPage() {
               <SearchButton />
             </div>
             <div>
-              <CampersList campers={campers} />
-              <LoadMoreBtn />
+              <CampersList campers={visibleCampers} />
+              {campersPerPage < campers.length && (
+                <LoadMoreBtn onClick={handleLoadMore} />
+              )}
             </div>
           </div>
         </>
