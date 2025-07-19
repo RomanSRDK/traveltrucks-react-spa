38c6bc49640be69cfc3d80b951d2a8680edb7008
Roman Serdiuk <roman.srdk21@gmail.com>
2025-07-19 10:18:27 -0400

    Features & Reviews

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,5 +1,81 @@
+import { useSelector } from "react-redux";
+import { selectСurrentItem } from "../../redux/campers/selectors";
+import CamperBookingForm from "../CamperBookingForm/CamperBookingForm";
+import css from "./Features.module.css";
+import sprite from "../../assets/sprite.svg";
+
 function Features() {
-  return <div>Features</div>;
+  const camper = useSelector(selectСurrentItem);
+  const {
+    form,
+    length,
+    width,
+    height,
+    tank,
+    consumption,
+    AC,
+    bathroom,
+    kitchen,
+    TV,
+    radio,
+    refrigerator,
+    microwave,
+    gas,
+    water,
+  } = camper;
+
+  const amenities = {
+    AC,
+    bathroom,
+    kitchen,
+    TV,
+    radio,
+    refrigerator,
+    microwave,
+    gas,
+    water,
+  };
+
+  const activeAmenities = Object.entries(amenities)
+    .filter(([key, value]) => value === true)
+    .map(([key]) => key);
+
+  return (
+    <>
+      <div className={css.camperFeatures}>
+        {activeAmenities.map((amenity) => (
+          <svg
+            key={amenity}
+            width="100"
+            height="48"
+            className={css.featureIcon}
+          >
+            <use href={`${sprite}#${amenity}`}></use>
+          </svg>
+        ))}
+      </div>
+      <ul>
+        <li>
+          <p>Form: {form}</p>
+        </li>
+        <li>
+          <p>Length: {length}</p>
+        </li>
+        <li>
+          <p>Width: {width}</p>
+        </li>
+        <li>
+          <p>Height: {height}</p>
+        </li>
+        <li>
+          <p>Tank {tank}</p>
+        </li>
+        <li>
+          <p>Consumption: {consumption}</p>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default Features;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,5 +1,27 @@
+import { useSelector } from "react-redux";
+import { selectСurrentItem } from "../../redux/campers/selectors";
+
 function Reviews() {
-  return <div>Reviews</div>;
+  const { reviews } = useSelector(selectСurrentItem);
+  console.log(reviews);
+
+  return (
+    <div>
+      {reviews.map(({ reviewer_name, reviewer_rating, comment }) => (
+        <ul>
+          <li>
+            <p>{reviewer_name}</p>
+          </li>
+          <li>
+            <p>{reviewer_rating}</p>
+          </li>
+          <li>
+            <p>{comment}</p>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
 }
 
 export default Reviews;
